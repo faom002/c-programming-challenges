b1b983544efc8bcfd754e1a8c3e8c4017572344b
faom002 <faisal_jobb@outlook.com>
2023-08-24 21:06:41 +0200

    continued working with sql

--- c-programming-tasks/projects/database_mysql/bookloaning/bookloaning.c
+++ c-programming-tasks/projects/database_mysql/bookloaning/bookloaning.c
@@ -1,4 +1,5 @@
 #include <mysql/mysql.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,20 +8,17 @@ extern MYSQL *conn;
 extern MYSQL_RES *res;
 extern MYSQL_ROW row;
 
-char *avialibleBooks;
-int arrayOfBooks;
+char *availableBooks;
+int arrayOfBooks, input = 0;
 
-// Loaning a book from the database
 extern void show_books(void) {
-
-    avialibleBooks = malloc(100 * sizeof(char));
-
+    availableBooks = malloc(100 * sizeof(char));
 
     if (mysql_query(conn, "SELECT * FROM books")) {
         fprintf(stderr, "Query error: %s\n", mysql_error(conn));
         exit(1);
     }
-    
+
     res = mysql_store_result(conn);
 
     if (res == NULL) {
@@ -29,31 +27,35 @@ extern void show_books(void) {
     }
 
     while ((row = mysql_fetch_row(res)) != NULL) {
-        // Process and print the row data, for example:
         printf("Book ID: %s, Book Name: %s, Author: %s, Available: %s\n", row[0], row[1], row[2], row[3]);
-        
-        // copying the books into char pointer array
-        strcpy(avialibleBooks, row[3]);
+        strcpy(availableBooks, row[3]);
 
-        
+        sscanf(availableBooks, "%d", &arrayOfBooks);
+        printf("Initial Available: %d\n", arrayOfBooks);
+
+        while (1) {
+            printf("Enter book ID to loan (or anything else to exit): ");
+            if (scanf("%d", &input)) {
+                arrayOfBooks--;
+                printf("Remaining books: %i\n", arrayOfBooks);
+
+               
+                    char updateQuery[100];
+                    snprintf(updateQuery, sizeof(updateQuery), "UPDATE books SET availiblebooks = %d WHERE id = %s", arrayOfBooks, row[0]);
+                    if (mysql_query(conn, updateQuery)) {
+                        fprintf(stderr, "Update query error: %s\n", mysql_error(conn));
+                        exit(1);
+                    }
+                     if (arrayOfBooks == 0) {
+                    printf("No more books left\n");
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
     }
 
-
-        //converting the books to integers array
-        sscanf(avialibleBooks, "%d", &arrayOfBooks );
-
-
-       
-
-        	 printf("%i\n", arrayOfBooks);
-   
-
-    // free availible books from books
-    free(avialibleBooks);
-
-    // free mysql res
+    free(availableBooks);
     mysql_free_result(res);
 }
-
-
-// tomorrow i will try and find out how i can update the database by creating a function that does something
