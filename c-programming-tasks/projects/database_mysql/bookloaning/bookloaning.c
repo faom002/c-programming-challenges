3f70d1acb991b4583eb1258d8d989f031af1055a
faom002 <faisal_jobb@outlook.com>
2023-08-30 20:47:28 +0200

    worked with bookloaning system

--- c-programming-tasks/projects/database_mysql/bookloaning/bookloaning.c
+++ c-programming-tasks/projects/database_mysql/bookloaning/bookloaning.c
@@ -9,28 +9,33 @@ extern MYSQL_RES *res;
 extern MYSQL_ROW row;
 
 char *inputAccept = NULL;
-int arrayOfBooks, input = 0, bookId = 0;
+int arrayOfBooks, input = 0;
 
 void update_rows() {
     printf("Enter book ID to loan (or any other input to exit): ");
     if (scanf("%d", &input)) {
         arrayOfBooks--;
-        printf("Remaining books: %d\n", arrayOfBooks);
+
+           if (arrayOfBooks < 0) {
+            printf("No more books left for that row\n");
+            exit(1);  
+        }else {
+            printf("Remaining books: %d\n", arrayOfBooks);
+        }
 
         char updateQuery[100];
-        snprintf(updateQuery, sizeof(updateQuery), "UPDATE books SET availiblebooks = %d WHERE id = %d", arrayOfBooks, 1);
+        snprintf(updateQuery, sizeof(updateQuery), "UPDATE books SET availiblebooks = %d WHERE id = %d", arrayOfBooks, input);
         if (mysql_query(conn, updateQuery)) {
             fprintf(stderr, "Update query error: %s\n", mysql_error(conn));
             exit(1);
         }
-        if (arrayOfBooks <= 0) {
-            printf("No more books left for that row\n");
-            exit(0);  
-        }
-    } else {
-    exit(0);
+     
+    }else {
+        exit(0);
     } 
 }
+
+
 
 extern void show_books(void) {
     if (mysql_query(conn, "SELECT * FROM books")) {
@@ -57,16 +62,14 @@ extern void show_books(void) {
 
     scanf("%s", inputAccept);
 
-    char *test = "yes";
+    char *accept = "yes";
 
     while (1) {
     
-    if (strcmp(inputAccept, test) == 0) {
+    if (strcmp(inputAccept, accept) == 0) {
         while ((row = mysql_fetch_row(res)) != NULL) {
             printf("Book ID: %s, Book Name: %s, Author: %s, Available: %s\n", row[0], row[1], row[2], row[3]);
             sscanf(row[3], "%d", &arrayOfBooks);
-            sscanf(row[0], "%d", &bookId);
-            printf("Initial Available: %d\n", arrayOfBooks);
 
 
         }
@@ -74,9 +77,10 @@ extern void show_books(void) {
 
             update_rows();
 
+    }else {
+        break;
     }
     }
-    printf("%d", bookId);
 
     free(inputAccept);
     mysql_free_result(res);
