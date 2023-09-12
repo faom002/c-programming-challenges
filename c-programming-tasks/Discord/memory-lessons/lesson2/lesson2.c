26f8d8f87bc7601973a68f6aabc64fe0eeb6c21a
Faisal Omar <93327868+faom002@users.noreply.github.com>
2023-09-12 15:37:47 -0400

    Update lesson2.c

--- c-programming-tasks/Discord/memory-lessons/lesson2/lesson2.c
+++ c-programming-tasks/Discord/memory-lessons/lesson2/lesson2.c
@@ -8,27 +8,57 @@
     // - tip: memcpy might also be of help
 
 
-    #include <stdio.h>
     #include <stdbool.h>
+    #include <stddef.h>
+    #include <stdio.h>
+   
 
-    bool reading_a_binary_file(FILE *file){
-        
+    bool write_a_binary_file(FILE *file){
 
-     file = fopen("newProgram.bin", "rb");
+
+     file = fopen("newProgram.bin", "wb");
         
       if (file == NULL) {
-        printf("")
+        perror("Error opening binary file");
+        return false;
       }
         
+          int data[] = {1,2,3,4,5};
+          
+          /* 
+           the first argument is the data we want to write to the binary file
+           The second argument, sizeof(int), specifies the size of each individual element in the data array that you want to             write to the binary file. It indicates how many bytes should be written for each element. In this case, it assumes             that each element in the data array is of type int and is 4 bytes in size, which is common on many systems.
+           */
+
+          size_t element_written = fwrite(data,sizeof(int), sizeof(data)/sizeof(int), file );
+
+
+          if (element_written != sizeof(data)/ sizeof(int)) {
+          return false;
+          }
+
+      
+          fclose(file);
+          
+          printf("written successfully to a binary file\n");
+
+      return true;
+
     }
 
 
 
     int main(void){
       
+      FILE *file = NULL;
+      
+
+
+      write_a_binary_file(file); 
+
+
+
       return 0;
 
     }
   
-
-
