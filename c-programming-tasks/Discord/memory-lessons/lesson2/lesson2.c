f48d7dfea8614791c2f9cbca269d758b839ddbb9
Faisal Omar <faisalomar@Faisals-MacBook-Air.local>
2023-09-14 13:47:38 -0700

    added comments

--- c-programming-tasks/Discord/memory-lessons/lesson2/lesson2.c
+++ c-programming-tasks/Discord/memory-lessons/lesson2/lesson2.c
@@ -11,33 +11,51 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+// writing to the binary file
 bool write_binary_file() {
+
+  // creating the file to write to using (wb)
     FILE *file = fopen("newProgram.bin", "wb");
 
+    // checking for if the file exist 
     if (file == NULL) {
         perror("Error opening binary file for writing");
         return false;
     }
 
+
+    // creating the array we want to write to file 
     int data[] = {1, 2, 3, 4, 5};
+
+    // getting the length of the data
     size_t num_elements = sizeof(data) / sizeof(data[0]);
 
     // Write the binary data to the file
     size_t elements_written = fwrite(data, sizeof(data[0]), num_elements, file);
 
+    // close the file
     fclose(file);
 
+
+    // if the elements that is written is not the same as the length of num length then return false
     if (elements_written != num_elements) {
         printf("Error writing data to the binary file\n");
         return false;
     }
 
+
+
+    // else return true because we have written the data as binary
     printf("Data written successfully to a binary file\n");
+
+
     return true;
 }
 
 
 bool read_binary_file() {
+    
     FILE *file = fopen("newProgram.bin", "rb");
 
     if (file == NULL) {
@@ -45,7 +63,9 @@ bool read_binary_file() {
         return false;
     }
 
+    
     int data[5];
+    
     size_t num_elements = sizeof(data) / sizeof(data[0]);
 
     // Read the binary data from the file
@@ -59,9 +79,11 @@ bool read_binary_file() {
     }
 
     printf("Data read from binary file: ");
+    
     for (size_t i = 0; i < num_elements; i++) {
         printf("%d ", data[i]);
     }
+    
     printf("\n");
 
     return true;
@@ -72,13 +94,21 @@ int main(void) {
   // bool functions return values so we can use them in if statements
 
     if (write_binary_file()) {
+    
         if (read_binary_file()) {
+    
             printf("Binary file operations completed successfully.\n");
+    
         } else {
+    
             printf("Error reading from binary file.\n");
         }
+  
     } else {
+  
         printf("Error writing to binary file.\n");
+  
+
     }
 
     return 0;
