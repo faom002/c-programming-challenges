d45cb88d0bb2d4ad1ddbefca7c4411fb3f54f618
Faisal Omar <faisalomar@Faisals-MacBook-Air.local>
2023-09-14 13:35:22 -0700

    added assignment work

--- c-programming-tasks/Discord/memory-lessons/lesson2/lesson2.c
+++ c-programming-tasks/Discord/memory-lessons/lesson2/lesson2.c
@@ -1,4 +1,4 @@
-    // Assignment: instead of reading the data from a text file
+// Assignment: instead of reading the data from a text file
     // - use fopen to open a file and assume that it's a binary file
     //   - tip: this involves the 'b' flag and 'r' flag for binary/read
     // - instead of reading the data to put into the array through scanf,
@@ -7,58 +7,75 @@
     //   because char being just one byte large is making the task simpler than it needs to be
     // - tip: memcpy might also be of help
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-    #include <stdbool.h>
-    #include <stddef.h>
-    #include <stdio.h>
-   
+bool write_binary_file() {
+    FILE *file = fopen("newProgram.bin", "wb");
 
-    bool write_a_binary_file(FILE *file){
-
-
-     file = fopen("newProgram.bin", "wb");
-        
-      if (file == NULL) {
-        perror("Error opening binary file");
+    if (file == NULL) {
+        perror("Error opening binary file for writing");
         return false;
-      }
-        
-          int data[] = {1,2,3,4,5};
-          
-          /* 
-           the first argument is the data we want to write to the binary file
-           The second argument, sizeof(int), specifies the size of each individual element in the data array that you want to             write to the binary file. It indicates how many bytes should be written for each element. In this case, it assumes             that each element in the data array is of type int and is 4 bytes in size, which is common on many systems.
-           */
-
-          size_t element_written = fwrite(data,sizeof(int), sizeof(data)/sizeof(int), file );
-
-
-          if (element_written != sizeof(data)/ sizeof(int)) {
-          return false;
-          }
-
-      
-          fclose(file);
-          
-          printf("written successfully to a binary file\n");
-
-      return true;
-
     }
 
+    int data[] = {1, 2, 3, 4, 5};
+    size_t num_elements = sizeof(data) / sizeof(data[0]);
 
+    // Write the binary data to the file
+    size_t elements_written = fwrite(data, sizeof(data[0]), num_elements, file);
 
-    int main(void){
-      
-      FILE *file = NULL;
-      
+    fclose(file);
 
-
-      write_a_binary_file(file); 
-
-
-
-      return 0;
-
+    if (elements_written != num_elements) {
+        printf("Error writing data to the binary file\n");
+        return false;
     }
-  
+
+    printf("Data written successfully to a binary file\n");
+    return true;
+}
+
+bool read_binary_file() {
+    FILE *file = fopen("newProgram.bin", "rb");
+
+    if (file == NULL) {
+        perror("Error opening binary file for reading");
+        return false;
+    }
+
+    int data[5];
+    size_t num_elements = sizeof(data) / sizeof(data[0]);
+
+    // Read the binary data from the file
+    size_t elements_read = fread(data, sizeof(data[0]), num_elements, file);
+
+    fclose(file);
+
+    if (elements_read != num_elements) {
+        printf("Error reading data from the binary file\n");
+        return false;
+    }
+
+    printf("Data read from binary file: ");
+    for (size_t i = 0; i < num_elements; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+
+    return true;
+}
+
+int main(void) {
+    if (write_binary_file()) {
+        if (read_binary_file()) {
+            printf("Binary file operations completed successfully.\n");
+        } else {
+            printf("Error reading from binary file.\n");
+        }
+    } else {
+        printf("Error writing to binary file.\n");
+    }
+
+    return 0;
+}
